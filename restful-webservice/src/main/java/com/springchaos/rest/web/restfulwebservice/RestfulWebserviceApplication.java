f249c4064c444a714cbae98fa2dbaa312b39139e
Ravindra Prasad <ravindraprasad@yahoo.com>
2018-04-28 08:09:40 +0530

    Initial commit

---
+++ restful-webservice/src/main/java/com/springchaos/rest/web/restfulwebservice/RestfulWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.springchaos.rest.web.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebserviceApplication.class, args);
+	}
+}
